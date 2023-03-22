f6768400726c323524e816b41da8fb620735502b
Алексей Фомичев <122698243+Ololosh2332@users.noreply.github.com>
2023-03-22 19:33:05 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+    UserServiceImpl userService = new UserServiceImpl();
+    userService.dropUsersTable();
+    userService.createUsersTable();
+    userService.saveUser("ололошка", "Ололоев", (byte)12);
+        userService.saveUser("ололошка1", "Ололоев1", (byte)123);
+        userService.removeUserById(1);
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +16,65 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        try (Statement statement = Util.getConnection().createStatement();) {
+            statement.execute("CREATE TABLE users (id BIGINT auto_increment primary key, name varchar(50), lastname varchar(50), age TINYINT)");
+        } catch (SQLException e){
+        }
     }
 
     public void dropUsersTable() {
+        try (Statement statement = Util.getConnection().createStatement();) {
+            statement.execute("DROP TABLE users");
+        } catch (SQLException e){
 
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String request = "INSERT INTO users VALUES(null, '"+name+"' ,'"+lastName+"', "+age+")";
 
+
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.executeUpdate(request);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sqlRequest = "DELETE FROM users WHERE ID = ?";
+        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sqlRequest)) {
+            preparedStatement.setLong(1, id);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User>list = new ArrayList<>();
+        try (Statement statement = Util.getConnection().createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+            while (resultSet.next()){
+                long id = resultSet.getLong(1);
+                String name = resultSet.getString(2);
+                String lastName = resultSet.getString(3);
+                byte age = resultSet.getByte(4);
+                User user = new User(name, lastName, age);
+                user.setId(id);
+                list.add(user);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DELETE FROM users");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
